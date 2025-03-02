a5ba9ba39dcb72afe8bd04641fcbaa4f85097326
MannyD16 <139398471+MannyD16@users.noreply.github.com>
2025-03-01 22:05:09 -0500

    Initial commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Manuel Diaz on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
